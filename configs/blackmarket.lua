4d5278be5da3868110b9f28dfad7c0ff57f56a54
SagginChair <Vlezxsaggin1@gmail.com>
2021-11-18 20:23:58 -0600

    Create blackmarket.lua

---
+++ configs/blackmarket.lua
@@ -0,0 +1,41 @@
+
+
+-- front
+table.insert(Config.DoorList, {
+	doorID = 97,
+	objCoords = vector3(-597.2216, 210.058, 74.30254),
+	locked = true,
+	maxDistance = 2.0,
+	fixText = false,
+	lockpick = false,
+	objHash = 747286790,
+	authorizedJobs = { ['blackmarket']=0 },
+	audioRemote = false,
+	objHeading = 360.0,
+	slides = false,
+	garage = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
+
+-- back
+table.insert(Config.DoorList, {
+	doorID = 98,
+	objCoords = vector3(-592.907, 213.091, 73.1418),
+	locked = true,
+	maxDistance = 2.0,
+	fixText = false,
+	lockpick = false,
+	objHash = -1987474252,
+	authorizedJobs = { ['blackmarket']=0 },
+	audioRemote = false,
+	objHeading = 179.99998474121,
+	slides = false,
+	garage = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
