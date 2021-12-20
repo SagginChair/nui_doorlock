1c8d6df51a10e35760c443098dca2162fea40a69
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-20 17:52:08 -0600

    Create boostin.lua

---
+++ configs/boostin.lua
@@ -0,0 +1,21 @@
+
+
+-- mainboostindoor
+table.insert(Config.DoorList, {
+	locked = true,
+	authorizedJobs = { ['admin']=0 },
+	garage = false,
+	audioRemote = false,
+	fixText = false,
+	lockpick = false,
+	doorID = 106,
+	objHash = -710818483,
+	objHeading = 179.99998474121,
+	objCoords = vector3(1007.493, -3166.48, -38.75727),
+	slides = false,
+	maxDistance = 2.0,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
