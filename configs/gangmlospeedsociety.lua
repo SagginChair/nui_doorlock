6404630b3b4e68d4f1612490a18976bcdf8d64d3
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-29 03:27:17 -0600

    Create gangmlospeedsociety.lua

---
+++ configs/gangmlospeedsociety.lua
@@ -0,0 +1,61 @@
+
+
+-- garage1
+table.insert(Config.DoorList, {
+	fixText = false,
+	objHash = -2076929163,
+	garage = true,
+	doorID = 123,
+	locked = true,
+	lockpick = false,
+	slides = true,
+	objHeading = 0.0010702600702643,
+	authorizedGangs = { ['speedsociety']=0 },
+	audioRemote = false,
+	objCoords = vector3(935.9291, -1489.476, 31.16137),
+	maxDistance = 6.0,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
+
+-- garage2
+table.insert(Config.DoorList, {
+	fixText = false,
+	objHash = -2076929163,
+	garage = true,
+	doorID = 124,
+	locked = true,
+	lockpick = false,
+	slides = true,
+	objHeading = 0.0010702600702643,
+	authorizedGangs = { ['speedsociety']=0 },
+	audioRemote = false,
+	objCoords = vector3(943.3696, -1489.476, 31.17251),
+	maxDistance = 6.0,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
+
+-- frontdoor
+table.insert(Config.DoorList, {
+	fixText = false,
+	objHash = 1052098579,
+	garage = false,
+	doorID = 125,
+	locked = true,
+	lockpick = false,
+	slides = false,
+	objHeading = 0.0010702600702643,
+	authorizedGangs = { ['speedsociety']=0 },
+	audioRemote = false,
+	objCoords = vector3(939.0271, -1489.573, 30.242),
+	maxDistance = 2.0,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000
+})
